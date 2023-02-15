b7e86a1f216192b35200f4655383685d83ac7935
Vlad Andrei <vladalistar@gmail.com>
2023-02-15 13:12:33 +0200

    Divided the class methods in order to be easier to create new posts and understend what the code does

--- facebook_manager.py
+++ facebook_manager.py
@@ -1,23 +1,29 @@
 import os
 import requests
 
+
 class FacebookPoster:
-    """Class for posting content to a Facebook page.
+    """A class for posting content to a Facebook page.
 
     Attributes:
-        FACEBOOK_PAGE_ID (str): ID of the Facebook page.
-        PAGE_ACCESS_TOKEN (str): Access token of the Facebook page.
-        PHOTO_ENDPOINT (str): Endpoint URL for posting photos to the Facebook page.
-        VIDEO_ENDPOINT (str): Endpoint URL for posting videos to the Facebook page.
-        PAGE_FEED_ENDPOINT (str): Endpoint URL for posting to the Facebook page feed.
+        FACEBOOK_PAGE_ID (str): The ID of the Facebook page.
+        PAGE_ACCESS_TOKEN (str): The access token of the Facebook page.
+        PHOTO_ENDPOINT (str): The endpoint URL for posting photos to the Facebook page.
+        VIDEO_ENDPOINT (str): The endpoint URL for posting videos to the Facebook page.
+        PAGE_FEED_ENDPOINT (str): The endpoint URL for posting to the Facebook page feed.
     """
+    FACEBOOK_PAGE_ID: str
+    PAGE_ACCESS_TOKEN: str
+    PHOTO_ENDPOINT: str
+    VIDEO_ENDPOINT: str
+    PAGE_FEED_ENDPOINT: str
 
     def __init__(self, page_id: str, access_token: str) -> None:
-        """Inits FacebookPoster class with Facebook page ID and access token.
+        """Initializes a FacebookPoster object with the ID of the Facebook page and its access token.
 
         Args:
-            page_id (str): ID of the Facebook page.
-            access_token (str): Access token of the Facebook page.
+            page_id (str): The ID of the Facebook page.
+            access_token (str): The access token of the Facebook page.
         """
         self.FACEBOOK_PAGE_ID = page_id
         self.PAGE_ACCESS_TOKEN = access_token
@@ -25,52 +31,50 @@ class FacebookPoster:
         self.VIDEO_ENDPOINT = f"https://graph-video.facebook.com/{self.FACEBOOK_PAGE_ID}/videos"
         self.PAGE_FEED_ENDPOINT = f"https://graph.facebook.com/{self.FACEBOOK_PAGE_ID}/feed"
 
+    def __upload_image(self, image_path: str, image_description: str = None) -> dict:
+        """Uploads an image to Facebook. The image will not be published
+        on the page feed, but it will be embeddable in posts via its ID.
 
-    def upload_image(self, image_path: str, image_description: str = None):
-        """
-        Uploads an image to Facebook. The image will not be published on
-        the page Feed, however it will be embeddable in posts via its id.
-
-        Parameters:
+        Args:
             image_path (str): The path to the image to be uploaded or the URL of the image.
-            image_description (str, optional): The description to be added to the image. Default is None.
+            image_description (str, optional): The description to be added to the image. Defaults to None.
+
         Returns:
-            dict/None: The JSON response from the Facebook API or None if error.
+            dict: The JSON response from the Facebook API.
+
         Raises:
             Exception: If there is an error uploading the image, the error message is printed.
         """
-        params = {
+        data = {
             "access_token": self.PAGE_ACCESS_TOKEN,
             "message": image_description,
             "published": False
-        }  
+        }
         if os.path.isfile(image_path):
             files = {"file": open(image_path, "rb")}
         else:
             # We presume the string to be an image URL
             files = None
-            params["url"] = image_path
+            data["url"] = image_path
         try:
-            response = requests.post(self.PHOTO_ENDPOINT, files=files, params=params)
+            response = requests.post(
+                self.PHOTO_ENDPOINT, files=files, data=data)
+            response.raise_for_status()
         except Exception as e:
             print(f"Error uploading image to Facebook: {e}")
-            return None           
+            return None
         return response.json()
 
-    
-    def create_regular_post(self, post_text: str, image_paths: list = None, link: str = None):
+    def create_image_post(self, post_text: str, image_paths: list) -> dict:
         """
-        Create a new Facebook post (text only; text and images; text and link). If both image_paths
-        and link are declared, Facebook will ignore the uploaded images and will create a link post
-        (the post will have text and an embedded featured image from the linked article).
+        Creates a post with text and image(s) on the Facebook page. 
 
         Parameters:
             post_text (str): The text to be posted on Facebook.
-            image_paths (list of str, optional): A list of paths to images to be uploaded or URLs of images. Default is None.
-            link (str, optional): The URL to be attached to the post. Default is None.  
+            image_paths (list of str): A list of paths to images to be uploaded or URLs of images.
 
         Returns:
-            dict/None: The JSON response from the Facebook API or None if exception is raised.
+            dict or None: The JSON response from the Facebook API or None if exception is raised.
 
         Raises:
             Exception: If there is an error creating the post, the error message is printed.
@@ -79,28 +83,23 @@ class FacebookPoster:
             "access_token": self.PAGE_ACCESS_TOKEN,
             "message": post_text,
         }
-        # Since Facebook will ignore the images if link is present,
-        # we check to see if we should skip uploading the files
-        if image_paths and not link:
-            image_ids = []
-            for image_path in image_paths:
-                uploaded_image = self.upload_image(image_path)
-                if uploaded_image.get("id"):
-                    image_ids.append(uploaded_image["id"])
+        image_ids = []
+        for image_path in image_paths:
+            uploaded_image = self.__upload_image(image_path)
+            if uploaded_image.get("id"):
+                image_ids.append(uploaded_image["id"])
+        if image_ids:
             for index, image_id in enumerate(image_ids):
-                data[f"attached_media[{index}]"] = f"{{'media_fbid': '{image_id}'}}"       
-        if link:
-            data["link"] = link
+                data[f"attached_media[{index}]"] = f"{{'media_fbid': '{image_id}'}}"
         try:
             response = requests.post(self.PAGE_FEED_ENDPOINT, data=data)
         except Exception as e:
-            print(f"Error creating Facebook post: {e}")
+            print(f"Error creating Facebook post with image(s): {e}")
         return response.json()
 
-        
-    def create_video_post(self, video_path: str, title: str = None, description: str = None):
+    def create_video_post(self, video_path: str, title: str = None, description: str = None) -> dict:
         """
-        Posts a video to a Facebook page and adds a title and a description to it.
+        Creates a video post with a title and a description on the Facebook page.
 
         Parameters:
             video_path (str): Path to the video file.
@@ -108,20 +107,52 @@ class FacebookPoster:
             description (str, optional): The description to be added to the video post.
 
         Returns:
-            dict/None: JSON response from the Facebook Graph API with the ID of the newly posted video or None in case of error.
+            dict or None: JSON response from the Facebook Graph API with the ID of the newly posted video or None in case of error.
+
+        Raises:
+            Exception: If there is an error creating the post, the error message is printed.
         """
         files = {
-            "file": open (video_path, "rb")
+            "file": open(video_path, "rb")
         }
-        params = {
+        data = {
             "access_token": self.PAGE_ACCESS_TOKEN,
             "title": title,
             "description": description,
         }
         try:
-            response = requests.post(self.VIDEO_ENDPOINT, files=files, params=params)
+            response = requests.post(
+                self.VIDEO_ENDPOINT, files=files, data=data)
         except Exception as e:
             print(f"Error creating Facebook post with video: {e}")
             return None
         return response.json()
 
+    def create_text_post(self, text: str, url: str = None) -> dict:
+        """
+        Creates a text post on the Facebook page. 
+
+        If a URL is provided, the post will include the link and an image
+        that Facebook will try to extract from the linked resource.
+
+        Args:
+            text (str): The text content of the post.
+            url (str, optional): The URL to include in the post.
+
+        Returns:
+            dict or None: A dictionary containing the JSON response from the Facebook API, or None if an error occurred.
+
+        Raises:
+            Exception: If there is an error creating the post, the error message is printed.
+        """
+        data = {
+            "access_token": self.PAGE_ACCESS_TOKEN,
+            "message": text,
+            "link": url
+        }
+        try:
+            response = requests.post(self.PAGE_FEED_ENDPOINT, data=data)
+        except Exception as e:
+            print(f"Error creating Facebook text post: {e}")
+            return None
+        return response.json()
--- main.py
+++ main.py
@@ -7,7 +7,18 @@ load_dotenv()
 FACEBOOK_PAGE_ID = os.getenv("FACEBOOK_PAGE_ID")
 PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 fb = FacebookPoster(FACEBOOK_PAGE_ID, PAGE_ACCESS_TOKEN)
-video = fb.create_video_post("crows.mp4")
-print(video)
-foto = fb.create_regular_post("O noua postare", ["caini.jpg"])
-print(foto)
+
+text_post = fb.create_text_post("Text post with link", "https://example.com/")
+print(text_post)
+
+text_post_local_images = fb.create_image_post(
+    "Text post with local images", ["path_to_image1.jpg", "path_to_image2.jpg"])
+print(text_post_local_images)
+
+text_post_url_images = fb.create_image_post("Text post with remote images", [
+                                            "https://images.com/a.jpg", "https://images.com/b.jpg"])
+print(text_post_url_images)
+
+video_post = fb.create_video_post(
+    "path_to_video.mp4", "Video title", "Video description", )
+print(video_post)
